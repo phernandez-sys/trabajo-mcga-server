74df2d027c8d335a09f6799e22e8851c1cabc367
Pablo Hernandez <hzpablog@gmail.com>
2019-11-01 20:10:00 -0300

    Create server with login endpoint

---
+++ src/index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+const myUser = ({
+  email: "test@gmail.com",
+  password: "123"
+})
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get("/", (req, res) => {
+res.status(200).send("server working")
+  })
+
+app.post("/login", (req, res) =>{
+  if(!req.body.email){
+    return res.status(400).send({
+      success: false,
+      message: "Email is required"
+    })
+  }
+    if(!req.body.password){
+      return res.status(400).send({
+        success: false,
+        message: "Password is required"
+      })
+
+  }
+  if(req.body.email !== myUser.email || req.body.password !== myUser.password ){
+    return res.status(400).send({
+      success: false,
+      message: "User not exist"
+    })
+  }
+
+ return res.status(200).send({
+   success: true,
+   message: "User logged successfully",
+   user: myUser,
+ })
+})
+app.listen(4000, ()=>{
+  console.log("server working on localhost:4000")
+})
